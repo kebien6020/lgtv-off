fb79f53e679c859c9b0c4b4d851283cd4c79ef1d
Kevin Peña <kevin.pena.prog@gmail.com>
2022-08-24 17:58:16 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+import Lgtv from 'lgtv2'
+import process from 'node:process'
+
+const lgtv = Lgtv({
+  url: 'ws://192.168.1.31:3000',
+})
+
+lgtv.on('connect', () => {
+  lgtv.request('ssap://system/turnOff', (_err, _res) => {
+    lgtv.disconnect()
+    process.exit(0)
+  })
+})
