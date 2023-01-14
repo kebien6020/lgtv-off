86d443f3a4d3cc2dc9a8e78d3d8a68e2bd0fed50
Kevin Peña <kevin.pena.prog@gmail.com>
2023-01-14 15:17:35 -0500

    Update ip

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import Lgtv from 'lgtv2'
 import process from 'node:process'
 
 const lgtv = Lgtv({
-  url: 'ws://192.168.1.31:3000',
+  url: 'ws://192.168.1.34:3000',
 })
 
 lgtv.on('connect', () => {
